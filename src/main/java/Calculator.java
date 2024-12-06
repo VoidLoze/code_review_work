ee7f9158091adb72c9a92cbbd51143a166533a8b
YariksBBshka <kulikov.iaroslav@yandex.ru>
2024-12-06 15:10:37 +0300

    kul edit

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,27 @@
 public class Calculator {
     public int add(int a, int b){
-        //TODO inser your realisation in method add
+        return a+b;
     }
     public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+        return a-b;
     }
-    public int div(int a, int b){
-        //TODO inser your realisation in method div
+    public int div(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Дaun?");
+        }
+        return a / b;
     }
-    public int times(int a, int b){
-        //TODO inser your realisation in method times
+
+
+    public int times(int a, int b) {
+        return a * b;
     }
-    public int solver(){
-        //TODO inser your realisation in method solver
+
+    public int solver() {
+        int result = add(10, 5); // 15
+        result = times(result, 2); // 30
+        result = div(result, 3); // 10
+        result = dif(result, 4); // 6
+        return result;
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,31 +1,23 @@
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.*;import static org.junit.jupiter.api.Assertions.assertEquals;
 class CalculatorTest {
-
+    Calculator calculator = new Calculator();
     @Test
     void add() {
-        //TODO inser your realisation in test-method add
+        assertEquals(10, calculator.add(5, 5));
     }
-
     @Test
     void dif() {
-        //TODO inser your realisation in test-method dif
+        assertEquals(2, calculator.dif(10,5));
     }
-
-    @Test
-    void div() {
-        //TODO inser your realisation in test-method div
+    @Test    void div() {
+        assertEquals(6, calculator.div(10,4));
     }
-
     @Test
     void times() {
-        //TODO inser your realisation in test-method times
+        assertEquals(10, calculator.times(9,2));
     }
-
-    @Test
-    void solver() {
-        //TODO inser your realisation in test-method solver
+    @Test    void solver() {
+        assertEquals(6, calculator.solver());
     }
 }
